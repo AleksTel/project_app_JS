3d73a8bf711dbd0e259f85ad55c2fb7d733457fb
Aleksandr <telegin@ua.fm>
2022-06-07 14:09:25 +0300

    second home work

--- script.js
+++ script.js
@@ -21,6 +21,20 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание2 на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
@@ -33,15 +47,49 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', '');
+// const b = prompt('На сколько оцените его?', '');
 
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+// const c = prompt('Один из последних просмотренных фильмов?', '');
+// const d = prompt('На сколько оцените его?', '');
+
+for (let i = 0; i < 2; i++) {
+    const latestWatchedMovies  = prompt('Один из последних просмотренных фильмов?', '');
+    const ratingMovies = prompt('На сколько оцените его?', '');
+
+    // const checkingForSpaces = latestWatchedMovies.replace(/\s/g,""); 
+
+    
+    if (latestWatchedMovies != null && ratingMovies != null && latestWatchedMovies != '' && ratingMovies != '' && latestWatchedMovies.length < 50) {
+        personalMovieDB.movies[latestWatchedMovies] = ratingMovies;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    // (!checkingForSpaces) ? (personalMovieDB.movies[latestWatchedMovies] = ratingMovies) : i--;
+   
+};
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+}else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+
+
+
+
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
